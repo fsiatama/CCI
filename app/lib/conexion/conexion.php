c733288651cb446c70c242f9e60bc2ed585f6cea
Fabian Siatama <fas0980@gmail.com>
2014-11-05 14:35:39 -0500

    mejoras en archivo de coneccion

--- app/lib/conexion/conexion.php
+++ app/lib/conexion/conexion.php
@@ -1,16 +1,33 @@
 <?php
-$ADODB_CACHE_DIR = PATH_RAIZ.'cache';
-define('ADODB_FORCE_VALUE',3); //PARA TOMAR LOS VALUES CON CADENA 'null' como NULL de mysql
-include(PATH_RAIZ.'adodb5/adodb.inc.php');	   # Carga el codigo comun de ADOdb
-class Conexion{
-    var $conn;
-    function Conexion($bd){
-		include(PATH_RAIZ.'lib/config.php');
-		$this->conn = &ADONewConnection('mysqli'); 
-    	$this->conn->PConnect($coneccion[$bd]['server'], $coneccion[$bd]['login'], $coneccion[$bd]['password'], $coneccion[$bd]['bd']);
+/**
+*@author Fabian Siatama
+*clase base para la coneccion a la base de datos
+*/
 
-		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC; 
-    }
+include(PATH_RAIZ.'adodb5/adodb.inc.php');
+
+class Conexion
+{
+	protected $conn;
+
+    public function __construct($bd)
+	{
+		$ADODB_CACHE_DIR = PATH_RAIZ.'cache';
+		define('ADODB_FORCE_VALUE',3); //PARA TOMAR LOS VALUES CON CADENA 'null' como NULL de mysql
+		$this->conn = ADONewConnection('mysqli');
+
+    	//print_r($bd);
+		require PATH_RAIZ.'lib/config.php';
+    	//$this->conn->Connect($bd['server'], $bd['login'], $bd['password'], $bd['bd']);
+    	$this->conn->Connect($coneccion[$bd]['server'], $coneccion[$bd]['login'], $coneccion[$bd]['password'], $coneccion[$bd]['bd']);
+
+		$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+	}
+	public function getConnection()
+	{
+		return $this->conn;
+	}
+
 }
 
 ?>
