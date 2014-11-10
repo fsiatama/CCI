608de522f3c15fca386613a2f7e54900c2278fa0
Fabian Siatama <fas0980@gmail.com>
2014-11-10 16:54:53 -0500

    finaliza jornada QH 10-11-2014

--- sql/min_agricultura_db.sql
+++ sql/min_agricultura_db.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Community v12.01 (64 bit)
+SQLyog Community v12.02 (64 bit)
 MySQL - 5.6.17 : Database - min_agricultura
 *********************************************************************
 */
@@ -16,6 +16,18 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`min_agricultura` /*!40100 DEFAULT CHARA
 
 USE `min_agricultura`;
 
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `category` */
+
 /*Table structure for table `indicator` */
 
 DROP TABLE IF EXISTS `indicator`;
@@ -28,6 +40,71 @@ CREATE TABLE `indicator` (
 
 /*Data for the table `indicator` */
 
+/*Table structure for table `menu` */
+
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `menu_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(45) NOT NULL,
+  `menu_category_menu_id` int(10) unsigned NOT NULL,
+  `menu_url` varchar(45) NOT NULL,
+  `menu_order` int(11) NOT NULL DEFAULT '1',
+  `menu_hidden` enum('0','1') NOT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `menu` */
+
+/*Table structure for table `permissions` */
+
+DROP TABLE IF EXISTS `permissions`;
+
+CREATE TABLE `permissions` (
+  `permissions_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permissions_profile_id` int(10) unsigned NOT NULL,
+  `permissions_menu_id` int(10) unsigned NOT NULL,
+  `permissions_list` enum('0','1') NOT NULL,
+  `permissions_modify` enum('0','1') NOT NULL,
+  `permissions_create` enum('0','1') NOT NULL,
+  `permissions_delete` enum('0','1') NOT NULL,
+  `permissions_export` enum('0','1') NOT NULL,
+  PRIMARY KEY (`permissions_id`),
+  UNIQUE KEY `permissions_profile_id` (`permissions_profile_id`,`permissions_menu_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `permissions` */
+
+/*Table structure for table `profile` */
+
+DROP TABLE IF EXISTS `profile`;
+
+CREATE TABLE `profile` (
+  `profile_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `profile_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`profile_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `profile` */
+
+insert  into `profile`(`profile_id`,`profile_name`) values (1,'Admin'),(2,'User');
+
+/*Table structure for table `session` */
+
+DROP TABLE IF EXISTS `session`;
+
+CREATE TABLE `session` (
+  `session_user_id` int(10) unsigned NOT NULL,
+  `session_php_id` varchar(45) NOT NULL,
+  `session_date` datetime NOT NULL,
+  `session_active` enum('0','1') NOT NULL,
+  PRIMARY KEY (`session_user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `session` */
+
+insert  into `session`(`session_user_id`,`session_php_id`,`session_date`,`session_active`) values (1,'lraurictrcf1dsgoe10tp3tnv0','2014-11-10 22:05:23','1');
+
 /*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
@@ -37,13 +114,17 @@ CREATE TABLE `user` (
   `user_full_name` varchar(255) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_password` char(35) NOT NULL,
+  `user_active` enum('0','1') NOT NULL,
+  `user_profile_id` int(10) unsigned NOT NULL,
   `user_uinsert` int(10) unsigned NOT NULL,
   `user_finsert` datetime NOT NULL,
   `user_fupdate` datetime NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_full_name`,`user_email`,`user_password`,`user_active`,`user_profile_id`,`user_uinsert`,`user_finsert`,`user_fupdate`) values (1,'FABIAN SIATAMA','fsiatama@sicex.com','202cb962ac59075b964b07152d234b70','1',0,1,'2014-11-07 10:44:48','0000-00-00 00:00:00');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
