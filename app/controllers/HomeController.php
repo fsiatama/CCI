6c8e58b17b73060558ef918d0dfda64f6ddb0cc6
memoher <julian_her@hotmail.com>
2015-01-25 23:06:44 -0500

    Mejoras en el Home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -16,52 +16,16 @@ class HomeController {
 
     public function guiaBasicaParaExportarAction() {
         $is_template = false;
-        return new View('guia_basica_para_exportar', compact('is_template'));
+        return new View('guia_basica', compact('is_template'));
     }
 
-    public function informacionGeneralTLCAction() {
+    public function informacionGeneralAction() {
         $is_template = false;
-        return new View('informacion_general_tlc', compact('is_template'));
+        return new View('informacion_general', compact('is_template'));
     }
 
-    public function requisitosIngresoColombiaAction() {
+    public function informacionExtraAction() {
         $is_template = false;
-        return new View('requisitos_ingreso_colombia', compact('is_template'));
+        return new View('informacion_extra', compact('is_template'));
     }
-
-    public function protocolosExportacionAction() {
-        $is_template = false;
-        return new View('protocolos_exportacion', compact('is_template'));
-    }
-
-    public function requisitosSanitariosFitosanitariosAction() {
-        $is_template = false;
-        return new View('requisitos_sanitarios_fitosanitarios', compact('is_template'));
-    }
-
-    public function losgisticaTransporteAction() {
-        $is_template = false;
-        return new View('logistica_transporte', compact('is_template'));
-    }
-
-    public function comercioAgropecuarioColombiaAction() {
-        $is_template = false;
-        return new View('comercio_agropecuario_colombia', compact('is_template'));
-    }
-
-    public function comercioAgropecuarioMundialAction() {
-        $is_template = false;
-        return new View('comercio_agropecuario_mundial', compact('is_template'));
-    }
-
-    public function colombiaPorAcuerdoComercialAction() {
-        $is_template = false;
-        return new View('colombia_por_acuerdo_comercial', compact('is_template'));
-    }
-
-    public function posicionamientoProductosAction() {
-        $is_template = false;
-        return new View('posicionamientoProductos', compact('is_template'));
-    }
-
 }
--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -12,63 +12,51 @@
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <div class="text-center">
-                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/aguacate.jpg" alt="First slide">
+                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/agricultor.jpg" alt="First slide">
             </div>
-            <!--<div class="container">
+            <div class="container">
                 <div class="carousel-caption">
                     <h1>Example headline.</h1>
                     <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
                     <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
                 </div>
-            </div>-->
+            </div>
         </div>
         <div class="item">
             <div class="text-center">
-                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/agricultor.jpg" alt="Second slide">
+                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/mundo1.jpg" alt="Third slide">
             </div>
-            <!--<div class="container">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>One more for good measure.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="text-center">
+                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/barco.jpg" alt="Second slide">
+            </div>
+            <div class="container">
                 <div class="carousel-caption">
                     <h1>Another example headline.</h1>
                     <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                     <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
                 </div>
-            </div>-->
+            </div>
         </div>
         <div class="item">
             <div class="text-center">
-                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/cafe.jpg" alt="Third slide">
+                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/cafe.jpg" alt="Four slide">
             </div>
-            <!--<div class="container">
+            <div class="container">
                 <div class="carousel-caption">
                     <h1>One more for good measure.</h1>
                     <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                     <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
                 </div>
-            </div>-->
-        </div>
-        <div class="item">
-            <div class="text-center">
-                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/cacao.jpg" alt="Four slide">
             </div>
-            <!--<div class="container">
-                <div class="carousel-caption">
-                    <h1>One more for good measure.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-                </div>
-            </div>-->
-        </div>
-        <div class="item">
-            <div class="text-center">
-                <img style="width: 100%;" src="<?= URL_RAIZ ?>img/carousel/papa.jpg" alt="Five slide">
-            </div>
-            <!--<div class="container">
-                <div class="carousel-caption">
-                    <h1>One more for good measure.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-                </div>
-            </div>-->
         </div>
     </div>
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
@@ -80,9 +68,6 @@
         <span class="sr-only">Next</span>
     </a>
 </div><!-- /.carousel -->
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('#myCarousel').parent('.container').removeClass('container');
-    });
-</script>
+<div>
+    <p></p>
+</div>
