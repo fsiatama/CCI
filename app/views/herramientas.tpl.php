cfaa9fdc6c87b7cfb5367103285fe8b78a18132f
memoher <julian_her@hotmail.com>
2015-01-26 22:36:41 -0500

    Completando el formulario de herramientas

--- app/views/herramientas.tpl.php
+++ app/views/herramientas.tpl.php
@@ -1,7 +1,59 @@
 <br />
 <div class="row">
-    <div class="col-md-12">
-        <h3>Protocolos de exportación vigentes por país</h3>
-        <iframe style="width: 100%; height: 600px;" src="http://www.ica.gov.co/getdoc/fdcf7ae0-72bd-4b00-a380-0608dc9f6033/Protocolos-Fitosanitarios.aspx"></iframe>
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong class="">Herramientas</strong>
+            </div>
+            <div class="panel-body">
+                    <h2>Protocolos de exportación</h2>
+                    <p>El siguiente link te permite consultar los protocolos de exportación vigentes por país</p>
+                    <ul>
+                        <li><a target="_blank" href="http://www.ica.gov.co/getdoc/fdcf7ae0-72bd-4b00-a380-0608dc9f6033/Protocolos-Fitosanitarios.aspx">Haz click aqui</a>
+                        </li>
+                    </ul>
+                    <hr>
+                    <h2>Requisitos sanitarios y fitosanitarios</h2>
+                    <p>También dispones de los siguientes links para consultar los requisitos sanitarios de paises como Estados Unidos y Canada</p>
+                    <ul>
+                        <li><a target="_blank" href="http://www.aphis.usda.gov/publications/plant_health/2012/importing_food_spanish.pdf">Estados Unidos</a></li>
+                        <li><a target="_blank" href="https://epermits.aphis.usda.gov/manual/index.cfm?NEW=1&REGION_ID=149&ACTION=countrySummCommPI">Estados Unidos</a></li>
+                        <li><a target="_blank" href="http://www.inspection.gc.ca/food/eng/1299092387033/1299093490225">Canada</a></li>
+                        <li><a target="_blank" href="http://exporthelp.europa.eu/thdapp/display.htm?page=rt/rt_RequisitosSanitariosYFitosanitarios.html&docType=main&languageId=ES">Union Europea</a></li>
+                    </ul>
+                    <hr>
+                    <h2>Logística de transporte</h2>
+                    <p>En el siguiente link podras consultar los médios de transporte disponibles y las rutas, además podras estimar el costo de cada uno</p>
+                    <ul>
+                        <li><a target="_blank" href="http://www.colombiatrade.com.co/herramientas/rutas-y-tarifas-de-transporte">Haz click aqui</a>
+                        </li>
+                    </ul>
+                    <hr>
+                    <h2>Utilización de contingentes</h2>
+                    <p>También dispones de los siguientes links para consultar los requisitos sanitarios de paises como Estados Unidos y Canada</p>
+                    <ul>
+                        <li><a target="_blank" href="http://www.cbp.gov/trade/quota/guide-import-goods/commodities">Estados Unidos</a></li>
+                        <li><a target="_blank" href="http://ec.europa.eu/taxation_customs/dds2/taric/quota_consultation.jsp?Lang=es&Screen=0&Status=&Offset=0&Critical=&Origin=&callbackuri=CBU-1&Code=&Expand=false">Unión Europea</a></li>
+                        <li>
+                            México
+                            <ul>
+                                <li><a target="_blank" href="http://www.siicex.gob.mx/portalSiicex/Transparencia/CUPOS.htm">Información general sobre cupos</a></li>
+                                <li><a target="_blank" href="http://www.siicex.gob.mx/portalSiicex/Transparencia/rendecue.htm">Utilización de cupos</a></li>
+                                <li><a target="_blank" href="http://www.siicex.gob.mx/portalSiicex/SICETECA/Acuerdos/Cupos/Cupos%20x%20PRODUCTO/Cupos%20por%20producto.htm">Cupos por producto</a></li>
+                            </ul>
+                        </li>
+                            <li><a target="_blank" href="http://www.ezv.admin.ch/dokumentation/04027/04028/05363/index.html?lang=fr">Efta</a></li>
+                    </ul>
+                    <hr>
+                    <h2>Comercio agropecuario mundial por producto</h2>
+                    <p>Podras acceder a la información del comercio mundial a través del siguiente link</p>
+                    <ul>
+                        <li><a target="_blank" href="http://www.trademap.org/Index.aspx?lang=es">Haz click aqui</a>
+                        </li>
+                    </ul>
+                    <hr>
+            </div>
+        </div>
     </div>
+
 </div>
